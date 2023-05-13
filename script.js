cc85ec3f2b2cee03555747879b740cedffc26e7b
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-05-13 22:49:45 +0530

    Update script.js
     edited the `script.js` file only

--- script.js
+++ script.js
@@ -8,20 +8,39 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+ let Morethan50 = arr.filter((mark) => mark.marks > 50).map((students) =>students.name);
+    console.log( Morethan50);
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+   arr.forEach((morethan50) =>{
+        if(morethan50.marks > 50){
+            console.log(morethan50.name);
+        }
+    });
 }
 
 function addData() {
   //Write your code here, just console.log
+   let newarr ={id:4,name:"shubham",age:"24",marks:80};
+    arr.push(newarr);
+    console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter((mark) => mark.marks >=50);
+    console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 =[
+        {id:5,name:"pankaj",age:"23",marks:95},
+        {id:6,name:"sana",age:"22",marks:75},
+        {id:7,name:"susan",age:"20",marks:45}
+    ];
+    arr = arr.concat(arr1);
+    console.log(arr);
 }
